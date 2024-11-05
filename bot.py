fa48e8d8a89ad0bf8284912084145d9b4d818e90
Mariya-Mukhitdinova <mukhitdinovamariya@gmail.com>
2024-11-05 16:29:35 +0500

    second

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import buttons as bt
+import database as db
 from geopy import Photon
 
 geolocator = Photon(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
@@ -15,11 +16,15 @@ def start(message):
 def get_name(message):
     user_id = message.from_user.id
     name = message.text
-    print(message.text)
     bot.send_message(user_id, f"{name}, Добро пожаловать в бот!")
-    bot.send_message(user_id, "Поделитесь своим контактом!",
-                     reply_markup=bt.phone_button())
-    bot.register_next_step_handler(message, get_phone_number, name)
+    check = db.check_user(user_id)
+    if check == True:
+        bot.send_message(user_id, f"{name}, Вы уже зарегистрированы!")
+    elif check == False:
+        print(message.text)
+        bot.send_message(user_id, f"{name}, Добро пожаловать в бот!")
+        bot.send_message(user_id, "Поделитесь своим контактом!", reply_markup=bt.phone_button())
+        bot.register_next_step_handler(message, get_phone_number, name)
 
 def get_phone_number(message, name):
     user_id = message.from_user.id
@@ -31,6 +36,8 @@ def get_phone_number(message, name):
     else:
         bot.send_message(user_id, "Отправьте свой номер через кнопку!",  reply_markup=bt.phone_button())
         bot.register_next_step_handler(message, get_phone_number,name)
+
+
 def location(message, name, phone_number):
     user_id = message.from_user.id
     if message.location:
@@ -38,10 +45,12 @@ def location(message, name, phone_number):
         longitude = message.location.longitude
         address = geolocator.reverse((latitude,longitude)).address
         print(name, phone_number, address)
+        db.add_user(name=name, phone_number=phone_number, user_id=user_id)
         bot.send_message(user_id, "Вы успешно зарегистрировались!")
 
     else:
-        bot.send_message(user_id, "Отправьте свою локацию через кнопку!",reply_markup=bt.location_button())
+        bot.send_message(user_id, "Отправьте свою локацию через кнопку!", reply_markup=bt.location_button())
         bot.register_next_step_handler(message, location, name, phone_number)
+
 
 bot.infinity_polling()
---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3
+connection = sqlite3.connect("my_bot.db")
+sql = connection.cursor()
+
+sql.execute("CREATE TABLE IF NOT EXISTS users "
+            "(name TEXT, user_id INTEGER,"
+            "phone_number TEXT);")
+
+connection.commit()
+
+def add_user(name,user_id, phone_number):
+    connection = sqlite3.connect("my_bot.db")
+    sql = connection.cursor()
+    sql.execute("INSERT INTO users (name, user_ud, phone_number) "
+                "VALUES (?, ?, ?);", (name, user_id, phone_number))
+    connection.commit()
+
+def check_user(user_id):
+    connection = sqlite3.connect("my_bot.db")
+    sql = connection.cursor()
+    check = sql.execute("SELECT * FROM users WHERE user_id=?", (user_id, )).fetchone()
+    if check:
+        return True
+    elif not check:
+        return False
+    connection.commit()

f022c87a70e4b6bde593feb77fdad96d58bfdd31
Mariya-Mukhitdinova <mukhitdinovamariya@gmail.com>
2024-11-08 10:48:33 +0500

    ready

--- bot.py
+++ bot.py
@@ -2,36 +2,34 @@ import telebot
 import buttons as bt
 import database as db
 from geopy import Photon
-import logging
-from aiogram import Bot, Dispatcher, executor, types
-from translate import Translator
 
-logging.basicConfig(level=logging.INFO)
 geolocator = Photon(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
 bot = telebot.TeleBot(token="7929424594:AAG80zGkZVal2so9Vf6DhUAhZjWK1ArwxSI")
-dp = Dispatcher(bot)
-
-ru_letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-uz_letters = "аbdеfghijklmnоpqrstuvxyzoʻgʻshchng"
-
 @bot.message_handler(commands=["start"])
+def language(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id,"Выберите язык", reply_markup=bt.lang())
+    bot.register_next_step_handler(message, start)
 def start(message):
     user_id = message.from_user.id
-    bot.send_message(user_id,"Введите своё имя для регистрации")
+    bot.send_message(user_id, "Введите своё имя для регистрации")
+    bot.send_message(user_id, "Ro'yxatdan o'tish uchun ismingizni kiriting")
     print(message.text)
     bot.register_next_step_handler(message, get_name)
 
 def get_name(message):
     user_id = message.from_user.id
     name = message.text
-    bot.send_message(user_id, f"{name}, Добро пожаловать в бот!")
     check = db.check_user(user_id)
     if check == True:
         bot.send_message(user_id, f"{name}, Вы уже зарегистрированы!")
+        bot.send_message(user_id, f"{name}, Siz allaqachon ro'yxatdan o'tgansiz!")
     elif check == False:
         print(message.text)
-        bot.send_message(user_id, f"{name}, Добро пожаловать в бот!")
-        bot.send_message(user_id, "Поделитесь своим контактом!", reply_markup=bt.phone_button())
+        bot.send_message(user_id, f"{name}, Добро пожаловать в бот!", reply_markup=bt.phone_button())
+        bot.send_message(user_id, f"{name}, Botga xush kelibsiz!", reply_markup=bt.phone_button())
+        # bot.send_message(user_id,  reply_markup=bt.phone_button())
+        # bot.send_message(user_id,  reply_markup=bt.phone_button())
         bot.register_next_step_handler(message, get_phone_number, name)
 
 def get_phone_number(message, name):
@@ -40,9 +38,11 @@ def get_phone_number(message, name):
         phone_number = message.contact.phone_number
         print(phone_number)
         bot.send_message(user_id, f"{name}, Поделитесь своей локацией!", reply_markup=bt.location_button())
+        bot.send_message(user_id, f"{name}, Joylashuvingizni baham ko'ring!", reply_markup=bt.location_button())
         bot.register_next_step_handler(message,location, name, phone_number)
     else:
         bot.send_message(user_id, "Отправьте свой номер через кнопку!",  reply_markup=bt.phone_button())
+        bot.send_message(user_id, "Raqamingizni tugma orqali yuboring!", reply_markup=bt.phone_button())
         bot.register_next_step_handler(message, get_phone_number,name)
 
 
@@ -53,33 +53,14 @@ def location(message, name, phone_number):
         longitude = message.location.longitude
         address = geolocator.reverse((latitude,longitude)).address
         print(name, phone_number, address)
-        db.add_user(name=name, phone_number=phone_number, user_id=user_id)
-        bot.send_message(user_id, "Вы успешно зарегистрировались!", button_translate)
 
+        bot.send_message(user_id, "Вы успешно зарегистрировались!")
+        bot.send_message(user_id, "Siz ro'yxatdan o'tdingiz!")
+        db.add_user(name=name, phone_number=phone_number, user_id=user_id)
     else:
         bot.send_message(user_id, "Отправьте свою локацию через кнопку!", reply_markup=bt.location_button())
+        bot.send_message(user_id, "Joylashuvingizni tugma orqali yuboring!", reply_markup=bt.location_button())
         bot.register_next_step_handler(message, location, name, phone_number)
-
-@bot.message_handler(commands=['Translator'])
-def button_translate(message):
-    user_id = message.from_user.id
-    bot.send_message(user_id, bt.translate())
-    bot.register_next_step_handler(message, translator)
-def translator(message):
-    user_id = message.from_user.id
-    text = message.text
-    if text[0].lower() in ru_letters:
-        translator = Translator(from_lang="russian", to_lang="uzbek")
-        translation = translator.translate(message.text)
-        print(translation)
-        bot.send_message(user_id, translation)
-    # translator = Translator(from_lang="russian", to_lang="uzbek")
-    elif text[0].lower() in uz_letters:
-        translator = Translator(from_lang="uzbek", to_lang="russian")
-        translation = translator.translate(message.text)
-        print(translation)
-        bot.send_message(user_id, translation)
-
 
 
 bot.infinity_polling()
--- buttons.py
+++ buttons.py
@@ -1,19 +1,27 @@
 from telebot import types
 
+def lang():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton("RU")
+    button1 = types.KeyboardButton("UZ")
+    kb.add(button)
+    kb.add(button1)
+    return kb
+
+
 
 def phone_button():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton("Поделиться контактом!", request_contact=True)
+    button1 = types.KeyboardButton("Aloqani baham ko'ring", request_contact=True)
     kb.add(button)
+    kb.add(button1)
     return kb
 def location_button():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton("Поделиться локацией!", request_location=True)
+    button1 = types.KeyboardButton("Joylashuvni ulashish!", request_location=True)
     kb.add(button)
+    kb.add(button1)
     return kb
 
-def translate():
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton("Переводчик")
-    kb.add(button)
-    return kb
